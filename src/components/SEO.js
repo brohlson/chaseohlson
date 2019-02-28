744f6a51ae73eb48086b387f32faee3b11bba1d1
Chase Ohlson <chase@chaseohlson.com>
2019-02-28 11:13:17 -0800

    update twitter card meta

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -64,7 +64,7 @@ function SEO({
               },
               {
                 name: `twitter:card`,
-                content: `summary`,
+                content: `summary_large_image`,
               },
               {
                 name: `twitter:creator`,
