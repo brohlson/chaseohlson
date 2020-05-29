7457857e3bbc33aafb00a24cc21e06f83c3fda65
brohlson <chase@chaseohlson.com>
2020-05-28 23:04:55 -0700

    chat

---
+++ src/html.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes} lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var _ctct_m = "ae78cccb979c9bb118a7d65acaa0848f";
+        `,
+          }}
+        />
+        <script
+          src="https://embed.small.chat/T51SWJUE9G514JHATS.js"
+          async
+        ></script>
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
